d4f50950ea5e670db09ad2a62fae967534b2cbf5
Udith Gayan Indrakantha <udith.indrakantha@gmail.com>
2020-12-21 13:10:37 +0530

    Init Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,138 +1,190 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div>
+    <button @click="getDoc">Generate Doc</button>
+    <br />
+    <input type="file" @change="handleFileChange"  />
+    <button @click="getPdf">Convert to pdf</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+/* eslint-disable */
+  import { Component, Prop, Vue } from "vue-property-decorator";
+  import docxtemplater from 'docxtemplater';
+  import JSZip from 'jszip';
+  import JSzipUtils from 'jszip-utils';
+  import  saveAs  from 'file-saver'; 
+  import convertToPdf from 'docx-pdf';
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+  @Component(
+    {
+      components: {
+        JSZip
+      }
+    }
+  )
+  export default class HelloWorld extends Vue {
+
+    fileDate: any;
+
+    handleFileChange(e: any){
+      this.fileDate = e.target.files[0];
+      console.log(this.fileDate);
+
+    }
+
+    getPdf() {
+      const form = new FormData();
+      form.append("Filecontent", this.fileDate);
+      
+      fetch("https://converter-docx-to-pdf.p.rapidapi.com/convertDOCXtoPDF", {
+      	"method": "POST",
+      	"headers": {
+      		"content-type": "multipart/form-data; boundary=---011000010111000001101001",
+      		"x-rapidapi-key": "a38c2d611bmsh044a92e5329b542p153171jsn729b4c074c87",
+      		"x-rapidapi-host": "converter-docx-to-pdf.p.rapidapi.com"
+        },
+        body: form
+      })
+      .then(response => {
+        const reader = response.body.getReader();
+        const stream = new ReadableStream({
+          start(controller) {
+            // The following function handles each data chunk
+            function push() {
+              // "done" is a Boolean and value a "Uint8Array"
+              reader.read().then(({ done, value }) => {
+                
+                // Is there no more data to read?
+                if (done) {
+                  // Tell the browser that we have finished sending data
+                  controller.close();
+                  return;
+                }
+
+                // Get the data and send it to the browser via the controller
+                controller.enqueue(value);
+                push();
+              });
+            };
+
+            push();
+          }
+        });
+
+        return new Response(stream, { headers: { "Content-Type": "application/pdf" } });
+      })
+      .then(response => response.blob())
+      .then(blob => {
+        // URL.createObjectURL(blob);
+        console.log(blob);
+        saveAs(blob, "newpdfff.pdf");
+        })
+      .catch(err => {
+      	console.error(err);
+      });
+    }
+
+    getDoc() {
+      this.createDOC();
+    }
+
+    loadFile(url: any,callback: any){
+        JSzipUtils.getBinaryContent(url,callback);
+    }
+
+    // loadFileFromLocal(url: any,callback: any){
+    //     return new Promise((resolve,reject) => {
+    //       const fr = new FileReader();
+    //       let file = fr.readAsArrayBuffer('template.docx');
+    //     });
+    // }
+
+    createDOC(){
+        let dataset = {
+          "clients": [
+            {
+              "first_name": "Udith",
+              "last_name": "Gayan",
+              "phone": "+44546546454"
+            },
+            {
+              "first_name": "Chamal",
+              "last_name": "Silva",
+              "phone": "+445476454"
+            }
+          ],
+          first_name: "John",
+          last_name: "Doe",
+          phone: "0652455478",
+          description: "New Website"
+        };
+
+        // this.loadFile("https://docxtemplater.com/tag-example.docx",function(error: any,content : any){
+          this.loadFile('doctemplates/template.docx',function(error: any,content : any){
+            if (error) { 
+              throw error
+               };
+
+            console.log(content);
+            let zip = new JSZip(content);
+            let doc = new docxtemplater().loadZip(zip)
+            doc.setData(dataset)
+
+            try {
+                doc.render()
+            }
+
+            catch (error) {
+                let e = {
+                    message: error.message,
+                    name: error.name,
+                    stack: error.stack,
+                    properties: error.properties,
+                }
+                console.log(JSON.stringify({error: e}));
+                // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
+                throw error;
+            }
+
+// docx generator
+            // let out = doc.getZip().generate({
+            //     type:"blob",
+            //     mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            //     })
+            // saveAs(out,`testbyudith.docx`);
+
+// pdf generator test
+            let out = doc.getZip().generate({
+                type:"blob",
+                mimeType: "application/pdf",
+                })
+
+             console.log(out) 
+             
+             let outt = new File([out], 'test.pdf')
+            saveAs(outt,`testtttbyudith.pdf`);        
+        });
+
+    }
+
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
---
+++ src/jsZip.d.ts
@@ -0,0 +1,14 @@
+declare module "jszip" {
+  import jszip from 'jszip';
+  export default JSZip;
+}
+
+declare module "jszip-utils" {
+  import JSZipUtils from 'jszip-utils.js';
+  export default JSZipUtils;
+}
+
+declare module "docx-pdf" {
+    import convertToPdf from 'docx-pdf';
+    export default convertToPdf;
+}
